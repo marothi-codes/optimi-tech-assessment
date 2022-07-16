493dbb83062e64487b5f91d12e77ef09cca7fb84
Marothi Mahlake <code_artist@outlook.com>
2022-07-16 16:52:52 +0200

    README.md updated to include install instructions.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Optimi Technical Assessment Project
+
+## Installation Instructions
+
+1. Clone the repo into your machine.
+2. Open a terminal in the repo directory.
+3. Run the command `npm install`.
+4. Within the already open terminal window run `npm start` once the installation finishes.
+5. Open a terminal for the frontend directory which is also in the repo.
+6. Run `npm install` for the frontend directory as well.
+7. Run `npm start` after the install finishes.
+8. A browser window will open, now you can test the application.
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -7,9 +7,7 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 import LiveSearch from './LiveSearch';
@@ -18,21 +16,13 @@ const pages = ['Products', 'Pricing', 'Blog', <LiveSearch />];
 
 const Navbar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
   };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
   };
 
   return (
