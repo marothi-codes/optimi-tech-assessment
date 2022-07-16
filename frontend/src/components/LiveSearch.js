a50c8815d2f7c6a58db43321f1bb103eede21494
Marothi Mahlake <code_artist@outlook.com>
2022-07-16 16:35:54 +0200

    Autocomplete feature complete.

--- frontend/src/components/LiveSearch.js
+++ frontend/src/components/LiveSearch.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Autocomplete from '@mui/material/Autocomplete';
-import ListSubheader from '@mui/material/ListSubheader';
 import { Box } from '@mui/system';
 import parse from 'autosuggest-highlight/parse';
 import match from 'autosuggest-highlight/match';
@@ -11,24 +11,43 @@ function LiveSearch() {
   const [projects, setProjects] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:4000/api/projects')
-      .then((response) => response.json())
-      .then((json) => setProjects(json.data));
-  });
+    (async function getProjects() {
+      const { data } = await axios.get('http://localhost:4000/api/projects');
+      setProjects(data);
+    })();
+  }, []);
+
+  // Material UI's Autocomplete component does not play well with the nested API data
+  // as it is due to conflicting props, so I will tweak the array ever so slightly.
+    const updatedProjects = projects.map(item => (
+      item.groups.map(g => ({ ...g, project: item.name })) // Add the project name property as a group object property so that groups may be grouped by project in the Autocomplete component.
+    ));
+
+    // Extract the groups & concatenate them into one array for the component.
+    const optionsList = [].concat(updatedProjects[0], updatedProjects[1]);
+
+    const handleSearch = (e) => {
+      const selectedGroup = optionsList.filter(x => x.name === e.target.value);
+      const urlToRedirectTo = selectedGroup[0].url;
+      window.open(urlToRedirectTo, '_blank');
+      e.target.value = null;
+    }
 
   return (
-    <Stack sx={{ width: 300, margin: 'auto' }}>
+    <Stack sx={{ width: 270, margin: 'auto' }}>
       <Autocomplete
-        id="project-search"
-        getOptionLabel={(project) => `${project.name} ${project.group.name}`}
-        options={projects}
-        sx={{ width: 300 }}
-        groupBy={(option) => option.name.toUpperCase()}
-        isOptionEqualToValue={(option, value) => option.group.name === value.group.name}
-        noOptionsText={'No projects with the specified name.'}
-        renderInput={(params) => <TextField {...params} placeholder="Search for a project" />}
-        renderOption={(props, project, { inputValue }) => {
-          const stringsToMatch = `${project.name} ${project.group.name}`;
+        id="nba-players"
+        autoHighlight={true}
+        groupBy={(group) => group.project}
+        getOptionLabel={(group) => group.name}
+        options={optionsList}
+        sx={{ width: 270 }}
+        isOptionEqualToValue={(option, value) => option.name === value.name}
+        noOptionsText={'No groups with the specified name.'}
+        renderInput={(params) => <TextField {...params} placeholder="Find a group." sx={{ backgroundColor: '#fff', borderRadius: '3.5px' }} />}
+        onChange={(e) => handleSearch(e)}
+        renderOption={(props, group, { inputValue }) => {
+          const stringsToMatch = `${group.name}`;
           const matches = match(
             stringsToMatch,
             inputValue,
@@ -38,7 +57,7 @@ function LiveSearch() {
           const parts = parse(stringsToMatch, matches);
 
           return (
-            <Box component="li" {...props} key={project.id}>
+            <Box component="li" {...props} key={group.id}>
               {parts.map((part, idx) => (
                 <span key={idx} style={{ backgroundColor: part.highlight ? '#ffff00' : '' }}>
                   {part.text}
@@ -52,4 +71,4 @@ function LiveSearch() {
   );
 }
 
-export default LiveSearch
+export default LiveSearch;
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,103 +1,132 @@
 import * as React from 'react';
-//import PropTypes from 'prop-types';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-//import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
 import LiveSearch from './LiveSearch';
 
-const drawerWidth = 240;
-const navItems = ['Home', 'About',<LiveSearch />, 'Contact'];
+const pages = ['Products', 'Pricing', 'Blog', <LiveSearch />];
 
-function DrawerAppBar(props) {
-  const { window } = props;
-  const [mobileOpen, setMobileOpen] = React.useState(false);
+const Navbar = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
   };
 
-  const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-      <Typography variant="h6" sx={{ my: 2 }}>
-        Optimi Project Manager
-      </Typography>
-        
-      <Divider />
-      <List>
-        {navItems.map((item) => (
-          <ListItem key={item} disablePadding>
-            <ListItemButton sx={{ textAlign: 'center' }}>
-              <ListItemText primary={item} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
-  const container = window !== undefined ? () => window().document.body : undefined;
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
   return (
-    <Box sx={{ display: 'flex' }}>
-      <AppBar component="nav">
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
-          >
-            {/* <MenuIcon //> */}Menu
-          </IconButton>
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
           <Typography
             variant="h6"
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
           >
-            Optimi Project Manager
+            Optimi
           </Typography>
-          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-            {navItems.map((item) => (
-              <Button key={item} sx={{ color: '#fff' }}>
-                {item}
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
               </Button>
             ))}
           </Box>
         </Toolbar>
-      </AppBar>
-      <Box component="nav">
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-        >
-          {drawer}
-        </Drawer>
-      </Box>
-      <Box component="main" sx={{ p: 3 }}>
-        <Toolbar />
-      </Box>
-    </Box>
+      </Container>
+    </AppBar>
   );
-}
-
-export default DrawerAppBar;
+};
+export default Navbar;
